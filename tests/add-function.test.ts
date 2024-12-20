2abcbdb3a7b8fa721abd0ed823cdf6ad31dce78e
pavan <pavan@inncircles.com>
2024-12-20 12:36:55 +0530

    added new test cases

--- tests/add-function.test.ts
+++ tests/add-function.test.ts
@@ -27,6 +27,10 @@ describe('Calculator Add Function', () => {
     expect(calculator.add('//;\n1;2')).toBe(3);
   });
 
+  it('should support multiple custom delimiters', () => {
+    expect(calculator.add('//[;][uu]\n1;2uu8')).toBe(11);
+  });
+
   it('should throw an exception for negative numbers', () => {
     expect(() => calculator.add('1,-2,3')).toThrow('Negatives not allowed: -2');
     expect(() => calculator.add('-1,-2')).toThrow('Negatives not allowed: -1, -2');
@@ -35,5 +39,18 @@ describe('Calculator Add Function', () => {
   it('should ignore numbers greater than 1000', () => {
     expect(calculator.add('2,1001')).toBe(2);
     expect(calculator.add('1000,1001')).toBe(1000);
+  });
+
+  it('should ignore empty and NaN values', () => {
+    expect(calculator.add('1,,,x,,2,3')).toBe(6);
+  });
+
+  it('should handle numbers with leading and trailing whitespace', () => {
+    expect(calculator.add(' 1 , 2 ,  6 ')).toBe(9);
+  });
+
+  it('should match special characters in delimiters', () => {
+    expect(calculator.add('//[$][^]\n1$2^6')).toBe(9);
+    expect(calculator.add('//|\n1|2|6')).toBe(9);
   });
 });
