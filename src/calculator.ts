6c7c424faadecdba3052f47d67b842f30605d44e
pavan <pavan@inncircles.com>
2024-12-27 15:04:16 +0530

    code refactoring

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,66 +1,20 @@
-import { escapeSpecialCharacters } from "./utils/helpers";
+import { escapeSpecialCharacters, parseInput, validateNumbers } from "./utils/helpers";
 
 export default class Calculator {
 
-    private singleDelimiterPattern = /^\/\/(.)\n/;
-    private multiDelimiterPattern = /^\/\/(\[.*?\])+\n/;
-    private defaultDelimiterPattern = /,|\n/;
-
     add(input: string) {
-        if (!input.trim()) return 0;
+        input = input.trim()
+        if (!input?.length) return 0;
 
         //default delimiter
-        let delimiterMatcher = this.defaultDelimiterPattern;
-
-        //check if custom delimiter input is provided
-        if(input.startsWith('//')){
-            const singleDelimiterMatch = input.match(this.singleDelimiterPattern);
-
-            // single character delimiter found
-            if (singleDelimiterMatch) {
-                const [delimiterSection, customDelimiter] = singleDelimiterMatch;
-                delimiterMatcher = new RegExp(escapeSpecialCharacters(customDelimiter));
-
-                // removing the delimiter declaration in input
-                input = input.substring(delimiterSection.length);
-            } else {
-                // check for multiple delimiters enclosed in square brackets
-                const multiDelimiterMatch = input.match(this.multiDelimiterPattern);
-
-                if(!multiDelimiterMatch?.length){
-                    throw new Error("Custom Delimiter Not Found")
-                }
-
-                // extract all delimiters enclosed in square brackets
-                const delimiterSection = multiDelimiterMatch[0];
-                const delimiters = [...delimiterSection.matchAll(/\[([^\]]+)\]/g)].map(([section, delimiter]) => escapeSpecialCharacters(delimiter));
-
-                // create a regular expression to match any of the delimiters
-                delimiterMatcher = new RegExp(delimiters.join('|'));
-
-                // removing the delimiter declaration in input
-                input = input.substring(delimiterSection.length);
-            }
-        }
+        let {rawInput, delimiterMatcher} = parseInput(input);
 
         // Split input based on the delimiter and filter out empty values
-        const numbers = input.split(delimiterMatcher)
+        const numbers = rawInput.split(delimiterMatcher)
             .map(item => Number(item))
-            .filter(num => !isNaN(num));
-
-        // Edge case: if no valid numbers exist after parsing
-        if (numbers.length === 0) {
-            return 0
-        }
-
-        // Edge case: check for negative numbers
-        const negatives = numbers.filter(num => num < 0);
-        if (negatives.length) {
-            throw new Error(`Negatives not allowed: ${negatives.join(', ')}`);
-        }
 
         // Edge case: ignore numbers greater than 1000
-        const validNumbers = numbers.filter(num => num <= 1000);
+        const validNumbers = validateNumbers(numbers);
 
         // Return the sum of valid numbers
         return validNumbers.reduce((sum, num) => sum + num, 0);
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,5 +1,61 @@
+//escapes special characters in regex
 export const escapeSpecialCharacters = (input: string) => {
     input = input.trim()
     input = input.replace(/[-[\]{}()*+?.,\\^$|#]/g, '\\$&')
     return input
+}
+
+const singleDelimiterPattern = /^\/\/(.)\n/;
+const multiDelimiterPattern = /^\/\/(\[.*?\])+\n/;
+const defaultDelimiterPattern = /,|\n/;
+
+//parses input and returns parsed raw input and delimter match regex
+export const parseInput = (input: string) => {
+    //default delimiter
+    let delimiterMatcher = defaultDelimiterPattern;
+
+    //check if custom delimiter input is provided
+    if(input.startsWith('//')){
+        const singleDelimiterMatch = input.match(singleDelimiterPattern);
+
+        // single character delimiter found
+        if (singleDelimiterMatch) {
+            const [delimiterSection, customDelimiter] = singleDelimiterMatch;
+            delimiterMatcher = new RegExp(escapeSpecialCharacters(customDelimiter));
+
+            // removing the delimiter declaration in input
+            input = input.substring(delimiterSection.length);
+        } else {
+            // check for multiple delimiters enclosed in square brackets
+            const multiDelimiterMatch = input.match(multiDelimiterPattern);
+
+            if(!multiDelimiterMatch?.length){
+                throw new Error("Custom Delimiter Not Found")
+            }
+
+            // extract all delimiters enclosed in square brackets
+            const delimiterSection = multiDelimiterMatch[0];
+            const delimiters = [...delimiterSection.matchAll(/\[([^\]]+)\]/g)].map(([section, delimiter]) => escapeSpecialCharacters(delimiter));
+
+            // create a regular expression to match any of the delimiters
+            delimiterMatcher = new RegExp(delimiters.join('|'));
+
+            // removing the delimiter declaration in input
+            input = input.substring(delimiterSection.length);
+        }
+    }
+    return {rawInput: input, delimiterMatcher}
+}
+
+export const validateNumbers = (numbers: number[]): number[] => {
+    numbers = numbers.filter(num => !isNaN(num));
+
+    //check for negative numbers
+    const negatives = numbers.filter(num => num < 0);
+    if (negatives.length) {
+        throw new Error(`Negatives not allowed: ${negatives.join(', ')}`);
+    }
+
+    const validNumbers = numbers.filter(num => num <= 1000);
+    return validNumbers
 }
